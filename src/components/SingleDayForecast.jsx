8d7db0016300194fd5628ecdfea7c3fd515c2095
Dinesh <dineshlegha23@gmail.com>
2024-05-07 14:08:37 +0530

    removed extra padding from 5 day forecast section

--- src/components/SingleDayForecast.jsx
+++ src/components/SingleDayForecast.jsx
@@ -2,16 +2,16 @@ import React from "react";
 
 const SingleDayForecast = ({ temp, date, day, img }) => {
   return (
-    <div className="flex justify-between items-center">
-      <div className="flex items-center gap-2">
+    <div className="flex  items-center">
+      <div className="flex items-center gap-2 w-full">
         <img src={img} alt="cloud logo" className="w-8" />
         <p className="text-xl font-semibold">
           {temp}
           <sup>o</sup>
         </p>
       </div>
-      <p className="text-white/50">{date}</p>
-      <p className="text-white/50">{day}</p>
+      <p className="text-white/50 w-full">{date}</p>
+      <p className="text-white/50 w-7/12">{day}</p>
     </div>
   );
 };
