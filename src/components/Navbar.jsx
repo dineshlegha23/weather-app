adf450142ab5ff7df756ddfaa640b8de7079d1e9
Dinesh <dineshlegha23@gmail.com>
2024-05-08 21:55:47 +0530

    added full screen search

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,13 @@ import { IoSearchOutline } from "react-icons/io5";
 import { FaLocationCrosshairs } from "react-icons/fa6";
 import { FaLocationDot } from "react-icons/fa6";
 import { useWeatherContext } from "../context/context";
+import { list } from "postcss";
 
 const Navbar = () => {
   const { city, setCity, cities, setCities, setCoordinates, setSelectedCity } =
     useWeatherContext();
   const [touched, setTouched] = useState(false);
+  const [mobileSearch, setMobileSearch] = useState(false);
 
   const fetchData = async () => {
     const response = await fetch(
@@ -27,11 +29,11 @@ const Navbar = () => {
   }, [city]);
 
   return (
-    <nav className="flex justify-between items-center py-5 mx-auto max-w-7xl md:flex-col md:gap-5">
+    <nav className="flex justify-between items-center py-5 mx-auto max-w-7xl">
       <i className="w-48">
         <img src={"/images/logo.png"} alt="logo" />
       </i>
-      <div onFocus={() => setTouched(true)} className="relative md:mx-auto">
+      <div onFocus={() => setTouched(true)} className="relative md:hidden">
         {touched && (
           <div
             onClick={() => setTouched(false)}
@@ -40,7 +42,7 @@ const Navbar = () => {
         )}
         <IoSearchOutline className="absolute top-[8px] ml-2" size={20} />
         <input
-          className="pl-9 py-[6px] bg-gray text-white outline-none w-[350px] px-2 rounded-full md:w-auto focus:rounded-none focus:rounded-t-[18px]"
+          className="md:hidden pl-9 py-[6px] bg-gray text-white outline-none w-[350px] px-2 rounded-full md:w-auto focus:rounded-none focus:rounded-t-[18px]"
           placeholder="Enter city name..."
           type="text"
           value={city}
@@ -73,9 +75,75 @@ const Navbar = () => {
           </ul>
         )}
       </div>
-      <div className="flex items-center relative gap-3 bg-[#B5A1F9] rounded-full py-[6px] px-3 cursor-pointer">
-        <FaLocationCrosshairs className="relative" />
-        <button className="">Current Location</button>
+      <div className="hidden md:flex md:gap-5">
+        <IoSearchOutline
+          onClick={() => setMobileSearch(true)}
+          size={20}
+          className="bg-gray rounded-full p-3 w-12 h-12"
+        />
+        <div className="flex items-center relative gap-3 bg-[#B5A1D9] rounded-full py-[6px] px-3 cursor-pointer md:p-1">
+          <FaLocationCrosshairs className="md:p-2 w-10 h-10" />
+        </div>
+      </div>
+      {mobileSearch && (
+        <div className="bg-black/100 fixed z-10 top-0 left-0 w-screen h-screen">
+          <div className="flex items-start mt-1 justify-center">
+            <button
+              className="py-3 px-5 bg-gray rounded-full mr-2 ml-1"
+              onClick={() => setMobileSearch(false)}
+            >
+              &lt;
+            </button>
+            <div className="flex flex-col w-[90%] mr-16">
+              <input
+                className={`pl-9 py-[10px] bg-gray text-white outline-none w-full px-2 rounded-full focus:rounded-none focus:rounded-t-[18px] ${
+                  (touched || cities.length > 0) &&
+                  "rounded-none rounded-t-[18px]"
+                }`}
+                placeholder="Enter city name..."
+                type="text"
+                value={city}
+                onFocus={() => setTouched(true)}
+                onBlur={() => setTouched(false)}
+                onChange={(e) => setCity(e.target.value)}
+              />
+              {cities && (
+                <ul
+                  className={`w-full shadow-[0px_10px_20px_black] z-10  flex flex-col bg-gray rounded-b-xl  ${
+                    touched || cities.length > 0 ? "border-t-[1px]" : ""
+                  } border-white/50`}
+                >
+                  {cities.map((city, index) => (
+                    <li
+                      onClick={(e) => {
+                        setCoordinates({ lat: city.lat, lon: city.lon });
+                        setSelectedCity(`${city.name}, ${city.country}`);
+                        setCity(city.name + " " + city.country);
+                        setMobileSearch(false);
+                      }}
+                      key={index}
+                      className="px-3 py-3 hover:bg-black/40 hover:cursor-pointer"
+                    >
+                      <div className="flex gap-3 items-center">
+                        <FaLocationDot size={20} color="gray" />
+                        <div>
+                          <p>{city.name}</p>
+                          <p className="text-white/50 leading-4">
+                            {city.state} {city.country}
+                          </p>
+                        </div>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </div>
+          </div>
+        </div>
+      )}
+      <div className="flex items-center relative gap-3 bg-[#B5A1D9] rounded-full py-[6px] px-3 cursor-pointer md:hidden">
+        <FaLocationCrosshairs className="relative md:p-1 w-8 h-8" />
+        <button>Current Location</button>
       </div>
     </nav>
   );
